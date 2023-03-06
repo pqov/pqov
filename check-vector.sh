be810a5850c13791ea49479417e996f6446c0f22
Matthias J. Kannwischer <matthias@kannwischer.eu>
2023-03-06 17:05:32 +0800

    fix check vector

--- check-vector.sh
+++ check-vector.sh
@@ -2,6 +2,8 @@
 
 rm -rf *.rsp
 ./PQCgenKAT_sign
+# make sure sha256sum is installed
+sha256sum *.rsp || exit 1
 hash=$(sha256sum  *.rsp | awk '{print $1}')
 hashparam="$hash $1 $2"
 if grep -q "$hashparam" SHA256SUMS
